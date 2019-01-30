d98c002b0f36287dd3cb19446d7cb948b804287e
Felix Zaussinger <felix.zaussinger@geo.tuwien.ac.at>
2019-01-30 14:24:27 +0100

    - commented 'sphinx.ext.pngmath' because it raises an extension error

--- docs/conf.py
+++ docs/conf.py
@@ -45,7 +45,9 @@ if on_rtd:
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.todo',
               'sphinx.ext.autosummary', 'sphinx.ext.viewcode', 'sphinx.ext.coverage',
-              'sphinx.ext.doctest', 'sphinx.ext.ifconfig', 'sphinx.ext.pngmath',
+              'sphinx.ext.doctest', 'sphinx.ext.ifconfig',
+              # commented the line below because it raises an extension error
+              #'sphinx.ext.pngmath',
               'sphinx.ext.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
@@ -115,7 +117,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
