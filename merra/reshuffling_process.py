6b7235cbe5ea664c5c8dce2f25148633cdc92d43
Felix Zaussinger <felix.zaussinger@geo.tuwien.ac.at>
2019-02-05 11:13:10 +0100

    - applied autopep8

--- merra/reshuffling_process.py
+++ merra/reshuffling_process.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 """
-Created on 03-02-2017 
+Created on 03-02-2017
 
 @author: fzaussin
 @email: felix.zaussinger@geo.tuwien.ac.at
@@ -15,26 +15,28 @@ from merra.reshuffle import reshuffle
 
 # data path definitions
 
-in_path = '/home/fzaussin/shares/radar/Datapool_raw/Earth2Observe/MERRA2/datasets/M2T1NXLND.5.12.4'
-out_path = '/home/fzaussin/shares/radar/Datapool_processed/Earth2Observe/MERRA2/datasets/M2T1NXLND.5.12.4_6hourly'
+in_path = '/home/fzaussin/shares/radar/Datapool_raw/' \
+          'Earth2Observe/MERRA2/datasets/M2T1NXLND.5.12.4'
+out_path = '/home/fzaussin/shares/radar/Datapool_processed/' \
+           'Earth2Observe/MERRA2/datasets/M2T1NXLND.5.12.4_6hourly'
 
 # define date range as datetime (!) objects
 start_date = datetime(1980, 1, 1)
 end_date = datetime(2018, 11, 30)
 
 # specific soil moisture params
-param_list = ['TSOIL1', 'TSOIL2', 'TSOIL3', 'TSOIL4', 'TSOIL5', 'TSOIL6', 'TSURF',
-              'SFMC', 'RZMC',
-              'GWETPROF', 'GWETROOT', 'GWETTOP',
+param_list = ['TSOIL1', 'TSOIL2', 'TSOIL3', 'TSOIL4', 'TSOIL5', 'TSOIL6',
+              'TSURF', 'SFMC', 'RZMC', 'GWETPROF', 'GWETROOT', 'GWETTOP',
               'SNOMAS', 'PRECSNOLAND', 'PRECTOTLAND']
 
 
 if __name__ == '__main__':
-    import time, datetime
+    import time
+    import datetime
 
     t1_start = time.perf_counter()
     t2_start = time.process_time()
-    
+
     # start process
     reshuffle(in_path=in_path,
               out_path=out_path,
@@ -43,7 +45,7 @@ if __name__ == '__main__':
               parameters=param_list,
               img_buffer=240,
               # specify time resolution
-              temporal_sampling=6) # one image every 6 hours
+              temporal_sampling=6)  # one image every 6 hours
 
     t1_stop = time.perf_counter()
     t2_stop = time.process_time()
