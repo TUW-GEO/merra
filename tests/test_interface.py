86515f6711e2e8340a124034db0acabbeadf1eb2
Felix Zaussinger <felix.zaussinger@geo.tuwien.ac.at>
2019-02-05 11:07:07 +0100

    - applied autopep8

--- tests/test_interface.py
+++ tests/test_interface.py
@@ -5,29 +5,34 @@ import numpy.testing as npt
 from datetime import datetime
 from merra.interface import MerraImage, MerraImageStack
 
+
 class Test(unittest.TestCase):
     """
     Testing base class
     """
+
     def test_img_reading_1D(self):
         """
         Test if netCDF image file is correctly read in 1D.
         """
         # specify parameters
-        parameters = ['SFMC', 'RZMC', 'GWETPROF', 'GWETROOT', 'GWETTOP', 'TSURF']
+        parameters = ['SFMC', 'RZMC', 'GWETPROF',
+                      'GWETROOT', 'GWETTOP', 'TSURF']
 
         # create image object based on test file
-        img = MerraImage(os.path.join(os.path.dirname(__file__),
-                                     'merra-test-data',
-                                     'M2T1NXLND.5.12.4',
-                                     '2018',
-                                     '10',
-                                     'MERRA2_400.tavg1_2d_lnd_Nx.20181001.nc4'),
-                         parameter=parameters,
-                         array_1D=True)
+        img = MerraImage(
+            os.path.join(
+                os.path.dirname(__file__),
+                'merra-test-data',
+                'M2T1NXLND.5.12.4',
+                '2018',
+                '10',
+                'MERRA2_400.tavg1_2d_lnd_Nx.20181001.nc4'),
+            parameter=parameters,
+            array_1D=True)
 
         # read image for specified timestamp
-        image = img.read(timestamp=datetime(2018,10,1,0,30))
+        image = img.read(timestamp=datetime(2018, 10, 1, 0, 30))
 
         # test assertions
         assert sorted(image.data.keys()) == sorted(parameters)
@@ -51,31 +56,34 @@ class Test(unittest.TestCase):
         Test if netCDF image file is correctly read in 2D.
         """
         # specify parameters
-        parameters = ['SFMC', 'RZMC', 'GWETPROF', 'GWETROOT', 'GWETTOP', 'TSURF']
+        parameters = ['SFMC', 'RZMC', 'GWETPROF',
+                      'GWETROOT', 'GWETTOP', 'TSURF']
 
         # create image object based on test file
-        img = MerraImage(os.path.join(os.path.dirname(__file__),
-                                     'merra-test-data',
-                                     'M2T1NXLND.5.12.4',
-                                     '2018',
-                                     '10',
-                                     'MERRA2_400.tavg1_2d_lnd_Nx.20181001.nc4'),
-                         parameter=parameters,
-                         array_1D=False)
+        img = MerraImage(
+            os.path.join(
+                os.path.dirname(__file__),
+                'merra-test-data',
+                'M2T1NXLND.5.12.4',
+                '2018',
+                '10',
+                'MERRA2_400.tavg1_2d_lnd_Nx.20181001.nc4'),
+            parameter=parameters,
+            array_1D=False)
 
         # read image for specified timestamp
-        image = img.read(timestamp=datetime(2018,10,1,0,30))
+        image = img.read(timestamp=datetime(2018, 10, 1, 0, 30))
 
         # test assertions
         assert sorted(image.data.keys()) == sorted(parameters)
         assert image.lon.shape == (361, 576)
         assert image.lon.shape == image.lat.shape
-        assert image.lon[0,0] == -180.0
-        assert image.lon[0,575] == 179.375
-        assert image.lat[0,0] == 90.0
-        assert image.lat[360,0] == -90.0
+        assert image.lon[0, 0] == -180.0
+        assert image.lon[0, 575] == 179.375
+        assert image.lat[0, 0] == 90.0
+        assert image.lat[360, 0] == -90.0
         # 361-277=84 (panoply seems to count latitude differently, although
-        # longitude is the same) we go from -90 to 90 and panoply from 0 to 180?
+        # longitude is the same) we go from -90-90 and panoply from 0-180?
         npt.assert_almost_equal(image.data['SFMC'][84][314], 0.218083,
                                 decimal=6)
         npt.assert_almost_equal(image.data['RZMC'][84][314], 0.218432,
@@ -94,13 +102,13 @@ class Test(unittest.TestCase):
         Test if the image stack is read correctly.
         """
         # specify parameters
-        parameters = ['SFMC', 'RZMC', 'GWETPROF', 'GWETROOT', 'GWETTOP',
-                      'TSURF']
+        parameters = ['SFMC', 'RZMC', 'GWETPROF',
+                      'GWETROOT', 'GWETTOP', 'TSURF']
 
         # create image stack object based on test file directory
         img = MerraImageStack(os.path.join(os.path.dirname(__file__),
-                                     'merra-test-data',
-                                     'M2T1NXLND.5.12.4'),
+                                           'merra-test-data',
+                                           'M2T1NXLND.5.12.4'),
                               parameter=parameters,
                               array_1d=True,
                               temporal_sampling=6)
@@ -132,26 +140,27 @@ class Test(unittest.TestCase):
         Test of timestamps are created correctly.
         """
         # specify parameters
-        parameters = ['SFMC', 'RZMC', 'GWETPROF', 'GWETROOT', 'GWETTOP',
-                      'TSURF']
+        parameters = ['SFMC', 'RZMC', 'GWETPROF',
+                      'GWETROOT', 'GWETTOP', 'TSURF']
 
         # create image stack object based on test file directory
         img = MerraImageStack(os.path.join(os.path.dirname(__file__),
-                                     'merra-test-data',
-                                     'M2T1NXLND.5.12.4'),
+                                           'merra-test-data',
+                                           'M2T1NXLND.5.12.4'),
                               parameter=parameters,
                               array_1d=True,
                               temporal_sampling=6)
 
         # create timestamps for the default 6-hourly sampling
-        tstamps = img.tstamps_for_daterange(start_date=datetime(2018,10,1),
-                                            end_date=datetime(2018,10,1))
+        tstamps = img.tstamps_for_daterange(start_date=datetime(2018, 10, 1),
+                                            end_date=datetime(2018, 10, 1))
         # test assertions
         assert len(tstamps) == 4
         assert tstamps == [datetime(2018, 10, 1, 0, 30),
                            datetime(2018, 10, 1, 6, 30),
                            datetime(2018, 10, 1, 12, 30),
                            datetime(2018, 10, 1, 18, 30)]
+
 
 if __name__ == "__main__":
     unittest.main()
