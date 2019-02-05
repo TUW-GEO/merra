0957e18eb4f757f2cc28565844f9c8cf4461a6a8
Felix Zaussinger <felix.zaussinger@geo.tuwien.ac.at>
2019-02-05 11:08:55 +0100

    - applied autopep8

--- tests/test_grid.py
+++ tests/test_grid.py
@@ -1,10 +1,12 @@
 import unittest
 from merra.grid import create_merra_cell_grid
 
+
 class Test(unittest.TestCase):
     """
     Testing base class
     """
+
     def test_MERRACellgrid(self):
         """
         Test if the grid remains homogenous.
@@ -16,6 +18,7 @@ class Test(unittest.TestCase):
         assert grid.activearrcell[159290] == 1431
         assert grid.activearrlat[159290] == 48.0
         assert grid.activearrlon[159290] == 16.25
+
 
 if __name__ == "__main__":
     unittest.main()
